076153e321d03d4e47b08c578e2634bf002cda51
EvansMutwiri <mutwirievansm@gmail.com>
2022-06-16 06:34:16 +0300

    Navbar and hero section redone

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,28 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero-img': "url('/src/assets/header-img.png')"
+        'hero-img': "url('/src/assets/header-img.png')",
+        'national-influence': "url('/src/assets/national-infl.png')",
       },
       fontFamily: {
         'Montserrat': ['Montserrat', 'sans-serif']
+      },
+      textUnderlineOffset: {
+        16: '16px',
       }
     },
 
-    fontSize: {
-      sm: ['14px', '20px'],
-      base: ['16px', '24px'],
-      lg: ['20px', '28px'],
-      xl: ['33px', '44px'],
-    }
+    colors: {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      faint: '#7e7e7f',
+      card: '#f5f5f5', 
+      gray: {
+        400: '#A1A1AA',
+        900: '#1a202c',
+      },
+    },
   },
   plugins: [],
 }
