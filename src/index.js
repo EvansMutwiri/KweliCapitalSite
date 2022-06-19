fe2ede22874c712361a4a9e33b8e1d746b5fbca3
EvansMutwiri <mutwirievansm@gmail.com>
2022-06-20 02:53:59 +0300

    navigation bar with scss styling and js conditional class toggling

---
+++ src/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+	document.querySelector('.hamburger').addEventListener("click", function() {
+		this.classList.toggle("hamburger--active");
+		document.querySelector(".nav-fullscreen").classList.toggle("nav-fullscreen--open");
+	});
+	
+});
