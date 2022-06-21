c63383a9ae4684c6d41170270cbfb2eadc226e21
EvansMutwiri <mutwirievansm@gmail.com>
2022-06-21 16:03:25 +0300

    solutions page image cover width

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 	
 });
+
+// var myNav = document.getElementById('nav');
+// window.onscroll = function () { 
+//     if (document.body.scrollHeight >= 600 ) {
+//         myNav.classList.add("bg-white");
+//         myNav.classList.remove("bg-transparent");
+// 		myNav.classList.add("text-black")
+//     } 
+//     else {
+//         myNav.classList.add("bg-transparent");
+//         myNav.classList.remove("bg-white");
+// 		myNav.classList.remove("text-black")
+//     }
+// };
